4a72ed7f41329f78073a778b8c849ac68df8f5ff
dwarakcheyyur <dwarak@cloudreign.in>
2018-01-10 16:50:35 +0530

    --

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -113,7 +113,7 @@ jQuery(document).ready(function($) {
     var $form = $('form.contactForm');
     url = 'https://script.google.com/a/education.cloudreign.in/macros/s/AKfycbxHiX2zzZwsPJtNmsiCWgCG-GvjHNaQwClAeswvlUr5FgZRPZ4/exec'
 
-$('#submit-form').on('click', function(e) {
+$('#submit').on('click', function(e) {
   e.preventDefault();
   var jqxhr = $.ajax({
     url: url,
