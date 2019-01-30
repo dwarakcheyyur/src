267a76a125b4daa2e612a8a78b3bd5f6a55bb589
dwarakcheyyur <dwarak@cloudreign.in>
2019-01-30 15:58:07 +0530

    changed digital classroom
    changed digital classroom

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -107,22 +107,3 @@ jQuery(document).ready(function($) {
             return false;
     });
 });
-
-
-/*
-var $form = $('form.contactForm');
-var url = 'https://script.google.com/a/education.cloudreign.in/macros/s/AKfycbxHiX2zzZwsPJtNmsiCWgCG-GvjHNaQwClAeswvlUr5FgZRPZ4/exec';
-
-$('#submit').on('click', function(e) {
-    e.preventDefault();
-    var jqxhr = $.ajax({
-        url: url,
-        method: "GET",
-        dataType: "json",
-        data: $form.serializeObject(),
-        success: function(msg){
-            console.log("success");
-        }
-    })
-});
-*/
